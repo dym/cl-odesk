63fb776be1529d6ed8e078ec58f5edf40fb8f19a
Dmitriy Budashny <dmitriy.budashny@gmail.com>
2011-03-12 17:23:58 +0200

    Added all authentication requests.

--- api/requests.lisp
+++ api/requests.lisp
@@ -2,6 +2,32 @@
 
 (in-package :odesk)
 
+;; Auth info
+; Example: (auth/get-frob *connection)
+(def-req auth/get-frob
+    (:url "keys/frobs"
+          :method :post)
+  "Get authentication frob.")
+
+; Example: (auth/get-token *connection* :params '(("frob" . "enter_here_frob")))
+(def-req auth/get-token
+    (:url "keys/tokens"
+          :method :post
+          :params params)
+  "Get authentication token.")
+
+; Example: (auth/check-token *connection*)
+(def-req auth/check-token
+    (:url "keys/token"
+          :method :get)
+  "Return authenticated user associated with authorization token.")
+
+; Example (auth/revoke-token *connection*)
+(def-req auth/revoke-token
+    (:url "keys/token"
+          :method :delete)
+  "Revoke given auth.")
+
 ;; Teams info
 ; Example: (team/get-teamrooms *connection*)
 (def-req team/get-teamrooms
@@ -11,7 +37,6 @@
   "Get all teamrooms accessible to the user.")
 
 ;; Profiles info
-
 ; Example: (profiles/get-providers *connection* :params '(("page" . "0;1") ("q" . "python")))
 (def-req profiles/get-providers
     (:url "search/providers"
